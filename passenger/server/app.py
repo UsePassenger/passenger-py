0b8501ec3a665fa57ae7c115795d21d77958678c
Ubuntu <ubuntu@ip-172-31-38-206.ec2.internal>
2018-08-16 19:37:25 +0000

    More DB options.

--- passenger/server/app.py
+++ passenger/server/app.py
@@ -48,9 +48,10 @@ def query():
     use_calendar = app.config['use_calendar']
     use_calendar_dates = app.config['use_calendar_dates']
     server_path = app.config['server_path']
+    dburl = app.config['dburl']
     db_path = os.path.join(server_path, 'gtfs.db')
 
-    db = database.APIDB(db_path, use_calendar=use_calendar, use_calendar_dates=use_calendar_dates)
+    db = database.APIDB(db_path, use_calendar=use_calendar, use_calendar_dates=use_calendar_dates, url=dburl)
     rows = db.query(departure, destination, daystamp)
 
     response = QueryResponse(
@@ -63,9 +64,10 @@ def query():
     return jsonify(response)
 
 
-def launch(server_path=os.path.expanduser('~/data/passenger-server/mnr'), host='0.0.0.0', port=5001):
+def launch(server_path=os.path.expanduser('~/data/passenger-server/mnr'), host='0.0.0.0', port=5001, url=None):
     app.config['server_path'] = server_path
     app.config['use_calendar'] = True
     app.config['use_calendar_dates'] = True
+    app.config['dburl'] = url
 
     app.run(debug=True, host=host, port=port)
--- scripts/server.py
+++ scripts/server.py
@@ -1,15 +1,32 @@
 import argparse
 
+from passenger.gtfs.database import get_url
 from passenger.server.app import launch
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+
+    # Network Parameters
     parser.add_argument('--port', type=int, default=5001)
     parser.add_argument('--host', type=str, default='0.0.0.0')
+
+    # DB Parameters
+    parser.add_argument('--username', default='passenger', type=str)
+    parser.add_argument('--password', default='passenger', type=str)
+    parser.add_argument('--dbhost', default='localhost', type=str)
+    parser.add_argument('--dbname', default='passenger', type=str)
+
     options = parser.parse_args()
+
+    url = get_url(
+        username=options.username,
+        password=options.password,
+        host=options.dbhost,
+        dbname=options.dbname)
 
     launch(
         host=options.host,
         port=options.port,
+        url=url,
         )
